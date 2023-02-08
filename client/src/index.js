9a8f6ed73dda8cc7bce4a506d96b0a6708dfeaaa
oguzhanalper06 <alperhanoguz06@gmail.com>
2023-02-08 22:55:41 +0300

    Hostta görüntüleme alınamadı.

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // Routeların çalışması için <App /> öğesini düzenlemeniz gerekir
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
